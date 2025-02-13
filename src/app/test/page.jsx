a2795aa3c04e86a865965afa2d52b8d82e4aadff
adarsh5534 <aipe3647@gmail.com>
2025-02-13 22:19:10 +0530

    build error fix again

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,28 +1,12 @@
 'use client';
-import CustomPagination from '@/components/CustomPagination';
-import React, { useState } from 'react';
 
 const Page = () =>
 {
-  const { state, dispatch } = useUser();
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = Math.ceil(state?.task_list?.totalPages / 5); // Replace with your actual total pages
-  console.log(state?.task_list?.pagination?.totalPages ,'sdddddddd')
+
   return (
     <main className="p-6 max-w-6xl mx-auto">
       <div className="flex justify-between items-center mb-6">
-        <CustomPagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          maxVisible={5}
-          onPageChange={setCurrentPage}
-        />
-      </div>
-      
-      {/* Your page content here */}
-      <div className="text-center">
-        Current Page: {currentPage}
-      </div>
+        </div>
     </main>
   );
 };
